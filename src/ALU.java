1f8a01832e2edd174945c09798560d8a5f62099c
Monkey Liang <i@aq741.com>
2016-05-22 12:32:37 +0800

    Update new framework

--- src/ALU.java
+++ src/ALU.java
@@ -315,7 +315,7 @@ public class ALU {
 	}
 	
 	/**
-	 * 4位先行进位加法器。<br/>
+	 * 4位先行进位加法器。要求采用{@link #fullAdder(char, char, char) fullAdder}来实现<br/>
 	 * 例：claAdder("1001", "0001", '1')
 	 * @param operand1 4位二进制表示的被加数
 	 * @param operand2 4位二进制表示的加数
@@ -329,7 +329,11 @@ public class ALU {
 	
 	/**
 	 * 加一器，实现操作数加1的运算。
-	 * 需要模拟{@link #fullAdder(char, char, char) fullAdder}来实现，但不可以调用{@link #fullAdder(char, char, char) fullAdder}。<br/>
+	 * 需要采用与门、或门、异或门等模拟，
+	 * 不可以直接调用{@link #fullAdder(char, char, char) fullAdder}、
+	 * {@link #claAdder(String, String, char) claAdder}、
+	 * {@link #adder(String, String, char, int) adder}、
+	 * {@link #integerAddition(String, String, int) integerAddition}方法。<br/>
 	 * 例：oneAdder("00001001")
 	 * @param operand 二进制补码表示的操作数
 	 * @return operand加1的结果，长度为operand的长度加1，其中第1位指示是否溢出（溢出为1，否则为0），其余位为相加结果
@@ -340,7 +344,21 @@ public class ALU {
 	}
 	
 	/**
-	 * 整数加法，要求调用{@link #claAdder(String, String, char) claAdder}方法实现。<br/>
+	 * 加法器，要求调用{@link #claAdder(String, String, char)}方法实现。<br/>
+	 * 例：adder("0100", "0011", ‘0’, 8)
+	 * @param operand1 二进制补码表示的被加数
+	 * @param operand2 二进制补码表示的加数
+	 * @param c 最低位进位
+	 * @param length 存放操作数的寄存器的长度，为4的倍数。length不小于操作数的长度，当某个操作数的长度小于length时，需要在高位补符号位
+	 * @return 长度为length+1的字符串表示的计算结果，其中第1位指示是否溢出（溢出为1，否则为0），后length位是相加结果
+	 */
+	public String adder (String operand1, String operand2, char c, int length) {
+		// TODO YOUR CODE HERE.
+				return null;
+	}
+	
+	/**
+	 * 整数加法，要求调用{@link #adder(String, String, char, int) adder}方法实现。<br/>
 	 * 例：integerAddition("0100", "0011", 8)
 	 * @param operand1 二进制补码表示的被加数
 	 * @param operand2 二进制补码表示的加数
@@ -353,7 +371,7 @@ public class ALU {
 	}
 	
 	/**
-	 * 整数减法，可调用{@link #integerAddition(String, String, char, int) integerAddition}方法实现。<br/>
+	 * 整数减法，可调用{@link #adder(String, String, char, int) adder}方法实现。<br/>
 	 * 例：integerSubtraction("0100", "0011", 8)
 	 * @param operand1 二进制补码表示的被减数
 	 * @param operand2 二进制补码表示的减数
@@ -366,7 +384,7 @@ public class ALU {
 	}
 	
 	/**
-	 * 整数乘法，使用Booth算法实现，可调用{@link #integerAddition(String, String, char, int) integerAddition}等方法。<br/>
+	 * 整数乘法，使用Booth算法实现，可调用{@link #adder(String, String, char, int) adder}等方法。<br/>
 	 * 例：integerMultiplication("0100", "0011", 8)
 	 * @param operand1 二进制补码表示的被乘数
 	 * @param operand2 二进制补码表示的乘数
@@ -379,7 +397,7 @@ public class ALU {
 	}
 	
 	/**
-	 * 整数的不恢复余数除法，可调用{@link #integerAddition(String, String, char, int) integerAddition}等方法实现。<br/>
+	 * 整数的不恢复余数除法，可调用{@link #adder(String, String, char, int) adder}等方法实现。<br/>
 	 * 例：integerDivision("0100", "0011", 8)
 	 * @param operand1 二进制补码表示的被除数
 	 * @param operand2 二进制补码表示的除数
@@ -392,8 +410,9 @@ public class ALU {
 	}
 	
 	/**
-	 * 带符号整数加法，要求调用{@link #integerAddition(String, String, int) integerAddition}、{@link #integerSubtraction(String, String, int) integerSubtraction}等方法实现。
-	 * 但符号的确定、结果是否修正等需要按照相关算法进行，不能直接转为补码表示后运算再转回来<br/>
+	 * 带符号整数加法，可以调用{@link #adder(String, String, char, int) adder}等方法，
+	 * 但不能直接将操作数转换为补码后使用{@link #integerAddition(String, String, int) integerAddition}、
+	 * {@link #integerSubtraction(String, String, int) integerSubtraction}来实现。<br/>
 	 * 例：signedAddition("1100", "1011", 8)
 	 * @param operand1 二进制原码表示的被加数，其中第1位为符号位
 	 * @param operand2 二进制原码表示的加数，其中第1位为符号位
@@ -406,7 +425,7 @@ public class ALU {
 	}
 	
 	/**
-	 * 浮点数加法，可调用{@link #integerAddition(String, String, char, int) intergerAddition}等方法实现。<br/>
+	 * 浮点数加法，可调用{@link #signedAddition(String, String, int) signedAddition}等方法实现。<br/>
 	 * 例：floatAddition("00111111010100000", "00111111001000000", 8, 8, 8)
 	 * @param operand1 二进制表示的被加数
 	 * @param operand2 二进制表示的加数
@@ -436,7 +455,7 @@ public class ALU {
 	}
 	
 	/**
-	 * 浮点数乘法，可调用{@link #integerAddition(String, String, char, int) integerAddition}等方法实现。<br/>
+	 * 浮点数乘法，可调用{@link #integerMultiplication(String, String, int) integerMultiplication}等方法实现。<br/>
 	 * 例：floatMultiplication("00111110111000000", "00111111000000000", 8, 8)
 	 * @param operand1 二进制表示的被乘数
 	 * @param operand2 二进制表示的乘数
@@ -450,7 +469,7 @@ public class ALU {
 	}
 	
 	/**
-	 * 浮点数除法，可调用{@link #integerAddition(String, String, char, int) integerAddition}等方法实现。<br/>
+	 * 浮点数除法，可调用{@link #integerDivision(String, String, int) integerDivision}等方法实现。<br/>
 	 * 例：floatDivision("00111110111000000", "00111111000000000", 8, 8)
 	 * @param operand1 二进制表示的被除数
 	 * @param operand2 二进制表示的除数
